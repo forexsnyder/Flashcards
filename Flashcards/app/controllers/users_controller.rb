393b2a209d52a6add3cb1e5246ff40b985ee7fcd
jvsnyder <forexsnyder@gmail.com>
2020-09-16 08:40:15 -0700

    auth v1

--- Flashcards/app/controllers/users_controller.rb
+++ Flashcards/app/controllers/users_controller.rb
@@ -25,15 +25,15 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    
+    if @user.save
+      @token = encode({id: @user.id})
+      render json: {
+        user: @user.attributes.except('password_digest'),
+        token: @token
+        }, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
