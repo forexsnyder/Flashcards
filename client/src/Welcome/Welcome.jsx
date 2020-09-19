39ef9f7ba84d976e55e624456417d555410a015c
forexsnyder <forexsnyder@gmail.com>
2020-09-19 07:21:56 -0700

    client update

---
+++ client/src/Welcome/Welcome.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, useRef } from 'react';
+import FlashcardList from './Component/FlashcardList';
+import './Welcome.css'
+
+function Welcome() {
+
+  
+  return (
+      <div>
+          <h1>Welcome to the welcome page</h1>
+      
+      
+      </div>
+   
+  );
+}
+
+export default Welcome;
