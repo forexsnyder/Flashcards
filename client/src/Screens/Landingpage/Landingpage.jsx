d84c5dae819b4b23ab4b5f17e9c3153aad8fc465
forexsnyder <forexsnyder@gmail.com>
2020-09-19 19:49:47 -0700

    landingpage axios

--- client/src/Screens/Landingpage/Landingpage.jsx
+++ client/src/Screens/Landingpage/Landingpage.jsx
@@ -1,82 +1,31 @@
 import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from '../../Component/FlashcardList';
-// import './App.css'
-import axios from 'axios'
-import Layout from "../../Component/Shared/Layout"
+import axios from 'axios';
+import Layout from "../../Component/Shared/Layout";
+import api from "../../Services/Api-config"
 
-function Landingpage() {
-  const [flashcards, setFlashcards] = useState([])
-  const [categories, setCategories] = useState([])
 
-  const categoryEl = useRef()
-  const amountEl = useRef()
+function Landingpage(){
+  const [topics,updateTopics] = useState()
 
-  useEffect(() => {
-    axios
-      .get('https://opentdb.com/api_category.php')
-      .then(res => {
-        setCategories(res.data.trivia_categories)
-      })
-  }, [])
+  useEffect(()=>{
+    const fetchTopic = async ()=> {
+      const resp= await api.get('/topics')
+      updateTopics(resp)
 
-  useEffect(() => {
-   
-  }, [])
+    }
+    fetchTopic()
 
-  function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML= str
-    return textArea.value
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    axios
-    .get('https://opentdb.com/api.php', {
-      params: {
-        amount: amountEl.current.value,
-        category: categoryEl.current.value
-      }
-    })
-    .then(res => {
-      setFlashcards(res.data.results.map((questionItem, index) => {
-        const answer = decodeString(questionItem.correct_answer)
-        const options = [
-          ...questionItem.incorrect_answers.map(a => decodeString(a)),
-          answer
-        ]
-        return {
-          id: `${index}-${Date.now()}`,
-          question: decodeString(questionItem.question),
-          answer: answer,
-          options: options.sort(() => Math.random() - .5)
-        }
-      }))
-    })
-  }
+
+  },[])
+
+
+
 
   return (
     <>
-      <form className="header" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="category">Category</label>
-          <select id="category" ref={categoryEl}>
-            {categories.map(category => {
-              return <option value={category.id} key={category.id}>{category.name}</option>
-            })}
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="amount">Number of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
-        </div>
-        <div className="form-group">
-          <button className="btn">Generate</button>
-        </div>
-      </form>
-      <div className="container">
-        <FlashcardList flashcards={flashcards} />
-      </div>
+
     </>
   );
 }
