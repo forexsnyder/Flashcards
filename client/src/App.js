6bbaafee07328d124fc1a32305542b3f49acf195
forexsnyder <forexsnyder@gmail.com>
2020-09-20 15:57:26 -0700

    new CUD Route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import CUDFlashcards from "./Screens/CUDFlashcards/CUDFlashcards"
 import Welcome from "./Screens/Welcome/Welcome"
 import Landingpage from "./Screens/Landingpage/Landingpage"
 import Flashcard from "./Screens/Flashcard/Flashcard"
+import CUDTopics from "./Screens/CUDFlashcards/CUDTopics"
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
         <Route exact path="/landingpage" component={Landingpage} />
         <Route exact path="/topics/:id/flashcards" component={Flashcard} />
         <Route exact path="/cudflashcards" component={CUDFlashcards} />
+        <Route exact path="/cudtopics" component={CUDTopics} />
         </Switch>
       </div>
   );
---
+++ client/src/Screens/CUDFlashcards/CUDTopics.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CUDFlashcards.css'
+import Layout from "../../Component/Shared/Layout"
+
+
+function CUDTopics() {
+
+  
+  return (
+      <Layout>
+        <div>
+      <h1>get ready to CUD Topics</h1>
+      
+      </div>
+      </Layout>
+  );
+}
+
+export default CUDTopics;
