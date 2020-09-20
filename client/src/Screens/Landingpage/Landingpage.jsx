467c158772ae2776eb679a36977e4a066a0ab610
forexsnyder <forexsnyder@gmail.com>
2020-09-20 13:28:32 -0700

    topics working properly

--- client/src/Screens/Landingpage/Landingpage.jsx
+++ client/src/Screens/Landingpage/Landingpage.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useParams, Link } from "react-router-dom";
 import FlashcardList from '../../Component/FlashcardList';
 import axios from 'axios';
 import Layout from "../../Component/Shared/Layout";
 import api from "../../Services/Api-config"
+import Topic from "../../Component/Topic/Topic"
 
 
 function Landingpage(){
-  const [topics,updateTopics] = useState()
+  const [topics,updateTopics] = useState([])
+  const params = useParams();
 
   useEffect(()=>{
     const fetchTopic = async ()=> {
       const resp= await api.get('/topics')
-      updateTopics(resp)
+      updateTopics(resp.data)
+      console.log(resp.data)
+
 
     }
     fetchTopic()
@@ -20,13 +25,24 @@ function Landingpage(){
 
   },[])
 
-
+  const topicsJSX = topics.map((topic, index) =>
+  <Topic id={topic.id} name={topic.name} key={index} />
+  
+  )
+//   const topicsJSX = topics.map((product, index) =>
+//   <Topic _id={product._id} name={product.name} imgURL={product.imgURL} price={product.price} key={index} />
+// )
 
 
   return (
-    <>
+    <div>
+      <div className="topics">
+      {topicsJSX}
+      </div>
 
-    </>
+    </div>
+
+    
   );
 }
 
