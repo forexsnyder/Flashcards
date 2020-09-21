7bce0a9202e0f35565314084ad79e1b7d2f43d43
forexsnyder <forexsnyder@gmail.com>
2020-09-20 21:21:58 -0700

    boiler plate

--- client/src/Screens/Flashcard/Flashcard.jsx
+++ client/src/Screens/Flashcard/Flashcard.jsx
@@ -1,42 +1,19 @@
-//modified from https://www.youtube.com/watch?v=hEtZ040fsD8
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react';
+import './Flashcard.css';
+import { Link } from 'react-router-dom'
 import Layout from "../../Component/Shared/Layout"
-export default function Flashcard({ flashcard }) {
-  const [flip, setFlip] = useState(false)
-  const [height, setHeight] = useState('initial')
 
-  const frontEl = useRef()
-  const backEl = useRef()
-
-  function setMaxHeight() {
-    const frontHeight = frontEl.current.getBoundingClientRect().height
-    const backHeight = backEl.current.getBoundingClientRect().height
-    setHeight(Math.max(frontHeight, backHeight, 100))
-  }
-
-  useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
-  useEffect(() => {
-    window.addEventListener('resize', setMaxHeight)
-    return () => window.removeEventListener('resize', setMaxHeight)
-  }, [])
-
-  return (
-    <Layout>
-    <div
-      className={`card ${flip ? 'flip' : ''}`}
-      style={{ height: height }}
-      onClick={() => setFlip(!flip)}
-    >
-      <div className="front" ref={frontEl}>
-        {flashcard.question}
-        <div className="flashcard-options">
-          {flashcard.options.map(option => {
-            return <div className="flashcard-option" key={option}>{option}</div>
-          })}
+const Flashcard = (props) => {
+    return (
+        <Layout>
+        <div>
+            <h1>Comment Section</h1>
         </div>
-      </div>
-      <div className="back" ref={backEl}>{flashcard.answer}</div>
-    </div>
-    </Layout>
-  )
+        <div>
+            <h1>Create new Comment</h1>
+        </div>
+        </Layout>
+    )
 }
+
+export default Flashcard
